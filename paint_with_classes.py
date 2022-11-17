702566c523d91eb780435a365186a9228b9f0436
biner1999 <biner1999@gmail.com>
2022-11-17 15:16:35 +0000

    Now includes multiple surfaces not to be painted on the wall

--- paint_with_classes.py
+++ paint_with_classes.py
@@ -1,6 +1,22 @@
 from math import ceil
 
+"""
+class Paint():
+    def __init__(self, brand, coverage, color):
+        self.brand = brand
+        self.coverage = coverage
+        self.color = color
 
+class BucketOfPaint(Paint):
+    def __init__(self, brand, coverage, color, volume, price):
+        super().__init__(brand, coverage, color)
+        self.volume = volume
+        self.price = price
+
+paint = Paint("Good Home", 13, "Brilliant White")
+bucket = BucketOfPaint(paint.brand, paint.coverage, paint.color, 2.5, 10)
+list_of_buckets = [BucketOfPaint(paint.brand, paint.coverage, paint.color, 2.5, 10)]
+"""
 class Surface():
     def __init__(self):
         self.height = 0
@@ -35,15 +51,24 @@ class Wall(Surface):
         super().__init__()
         self.name = "wall"
         self.coats = 0
+        self.surfaces_list = []
+        self.true_area = 0
 
+    def set_coats(self):
+        self.coats = int(input("How many coats of paint would your " + self.name + " need?\n"))
+        
+    def set_true_wall_area(self):
+        total_surfaces_area = 0
+        for surface in self.surfaces_list:
+            total_surfaces_area += surface.area
+        self.true_area = self.area - total_surfaces_area
+        
     def set_attributes(self):
         self.set_height()
         self.set_width()
         self.set_area()
         self.set_coats()
-        
-    def set_coats(self):
-        self.coats = int(input("How many coats of paint would your " + self.name + " need?\n"))
+    
 
 
 def calculate_liters_of_paint(area, coats, coverage):
@@ -53,26 +78,39 @@ def round_up_2dp(float):
     return ceil(float * 100) / 100
 
 
+buckets_of_paint = {2.5:12, 5:14, 10:20}
+
+
+
 def main():
     walls_list = []
     while True:
-        add_wall_inp = input(
-            "\nType 1 if you'd like to add a wall, type 0 if you're finished adding walls\n")
+        add_wall_inp = input("\nType 1 if you'd like to add a wall, type 0 if you're finished adding walls\n")
         match add_wall_inp:
             case "1":
                 wall = Wall()
                 wall.set_attributes()
+                while True:
+                    add_surf_inp = input("\nType 1 if you'd like to add a surface not to be painted on that wall, type 0 if you're finished adding surfaces\n")
+                    match add_surf_inp:
+                        case "1":
+                            surf = Surface()
+                            surf.set_attributes()
+                            wall.surfaces_list.append(surf)
+                        case "0":
+                            break
+                        case _:
+                            print("Wrong value inputted")
                 walls_list.append(wall)
             case "0":
                 total_liters = 0
                 for wall in walls_list:
-                    total_liters += round_up_2dp(calculate_liters_of_paint(wall.area, wall.coats, 13))
+                    wall.set_true_wall_area()
+                    total_liters += calculate_liters_of_paint(wall.true_area, wall.coats, 13)
                 print("To paint walls: ")
                 for wall in walls_list:
-                    print(str(wall.height) + " meters high by " + str(wall.width) +
-                          " meters wide with " + str(wall.coats) + " coats of paint")
-                print("You will need " + str(total_liters) +
-                      " liters of paint in total to paint all these walls")
+                    print(str(wall.height) + " meters high by " + str(wall.width) + " meters wide with " + str(wall.coats) + " coats of paint")
+                print("You will need " + str(total_liters) +" liters of paint in total to paint all these walls")
                 break
             case _:
                 print("Wrong value inputted")
