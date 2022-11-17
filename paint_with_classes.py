b2ed66a12a47cc17bc87cd1e291c698480c6cc34
biner1999 <biner1999@gmail.com>
2022-11-17 13:49:29 +0000

    Split the wall class to inherit from surface class

--- paint_with_classes.py
+++ paint_with_classes.py
@@ -1,23 +1,24 @@
 from math import ceil
 
 
-class Wall():
-    def __init__(self, height, width, coats):
+class Surface():
+    def __init__(self, height, width):
         self.height = height
         self.width = width
-        self.coats = coats
         self.area = self.get_area()
-        self.area_of_coats = self.get_area_of_coats()
-        self.liters_of_paint = self.get_liters_of_paint()
-        
+
     def get_area(self):
         return self.height * self.width
 
-    def get_area_of_coats(self):
-        return self.area * self.coats
-    
-    def get_liters_of_paint(self):
-        return self.area_of_coats/6
+
+class Wall(Surface):
+    def __init__(self, height, width, coats):
+        super().__init__(height, width)
+        self.coats = coats
+
+
+def calculate_liters_of_paint(area, coats, coverage):
+    return (area*coats)/coverage
 
 
 def round_up_2dp(float):
@@ -27,7 +28,8 @@ def round_up_2dp(float):
 def main():
     walls_list = []
     while True:
-        add_wall_inp = input("\nType 1 if you'd like to add a wall, type 0 if you're finished adding walls\n")
+        add_wall_inp = input(
+            "\nType 1 if you'd like to add a wall, type 0 if you're finished adding walls\n")
         match add_wall_inp:
             case "1":
                 h = float(input("How high is your wall in meters?\n"))
@@ -37,11 +39,13 @@ def main():
             case "0":
                 total_liters = 0
                 for wall in walls_list:
-                    total_liters += round_up_2dp(wall.liters_of_paint)
+                    total_liters += round_up_2dp(calculate_liters_of_paint(wall.area, wall.coats, 13))
                 print("To paint walls: ")
                 for wall in walls_list:
-                    print(str(wall.height) + " meters high by " + str(wall.width) + " meters wide with " + str(wall.coats) + " coats of paint")
-                print("You will need " + str(total_liters) + " liters of paint in total to paint all these walls")
+                    print(str(wall.height) + " meters high by " + str(wall.width) +
+                          " meters wide with " + str(wall.coats) + " coats of paint")
+                print("You will need " + str(total_liters) +
+                      " liters of paint in total to paint all these walls")
                 break
             case _:
                 print("Wrong value inputted")
